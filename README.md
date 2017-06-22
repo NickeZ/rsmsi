da200cf5f4a5dc5af7721720ba10148c2fd6bb05
Niklas Claesson <nicke.claesson@gmail.com>
2017-06-22 02:12:59 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [MSI](http://www.aps.anl.gov/epics/extensions/msi/index.php) implementation in rust for educational purpose.
+
+Requires Rust 1.19
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 extern crate clap;
 
+macro_rules! die {
+    ($($arg:tt)*) => {{
+        eprintln!($($arg)*);
+        ::std::process::exit(1);
+    }}
+}
+
 mod grammar;
 mod tmpl_grammar;
 
@@ -10,22 +17,36 @@ mod parser;
 mod lexer;
 
 use options::Options;
-use std::io::Read;
+use std::io::{Read, Write};
 use parser::expand_template;
+use std::fs::OpenOptions;
 
 fn main() {
-    let options = Options::parse_args().expect("Failed to parse options");
+    let options = Options::parse_args().unwrap_or_else(|_| die!("Failed to parse options"));
 
     let mut input = Vec::new();
     if let Some(template) = options.infile {
-        std::fs::File::open(template).unwrap().read_to_end(&mut input).unwrap();
+        let mut infile = std::fs::File::open(template).unwrap_or_else(|_| die!("Failed to open template file"));
+        infile.read_to_end(&mut input).unwrap_or_else(|_| die!("Failed to read from template file"));
     } else {
-        std::io::stdin().read_to_end(&mut input).unwrap();
+        std::io::stdin().read_to_end(&mut input).unwrap_or_else(|_| die!("Failed to read from stdin"));
     };
 
-    let res = expand_template(&String::from_utf8(input).unwrap(), &options.macros);
+    let input_as_utf8 = String::from_utf8(input).unwrap_or_else(|_| die!("Not valid utf8 in template file"));
 
-    print!("{}", res);
+    let res = expand_template(&input_as_utf8, &options.macros);
+
+    if let Some(outfile) = options.outfile {
+        let mut out = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(outfile)
+            .unwrap_or_else(|_| die!("Failed to open outfile for writing"));
+        out.write(res.as_bytes())
+            .unwrap_or_else(|_| die!("Failed to write to outfile"));
+    } else {
+        print!("{}", res);
+    }
     //println!("Sub {:?}", options.subfile);
     //println!("Makros {:?}", options.macros);
     //println!("Includes {:?}", options.includes);
